aac040beb2a7988e393b1352d8895d6de282e1cd
12 <12@DESKTOP-14UH3Q3>
2023-12-21 11:37:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 100;
+int n2 = (n % 100) / 10;
+int n3 = n % 10;
+if (Math.Pow(n, 2) == Math.Pow((n1 + n2 + n3), 3))
+{
+    Console.WriteLine("Условие истинно");
+}
+else
+{
+    Console.WriteLine("Условие ложно");
+}
